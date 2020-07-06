f13864de62a201e3c5572b833c50ecfd12718f1c
Jesus Esquer <jme2791@gmail.com>
2020-07-06 16:38:20 -0700

    pixelation over face working!

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const video = document.querySelector(".webcam");
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 
-const face = document.querySelector(".face");
-const faceCtx = face.getContext("2d");
+const faceCanvas = document.querySelector(".face");
+const faceCtx = faceCanvas.getContext("2d");
 
 const faceDetector = new window.FaceDetector();
-
-console.log(video, canvas, face, faceDetector);
-
+const options = {
+ SIZE = 10,
+ SCALE = 2,
+}
 // write a function that will populate the users video
 
 async function populateVideo() {
@@ -21,14 +22,15 @@ async function populateVideo() {
   // size canvas for same size as video
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
-  face.width = video.videoWidth;
-  face.height = video.videoHeight;
+  faceCanvas.width = video.videoWidth;
+  faceCanvas.height = video.videoHeight;
 }
 
 async function detect() {
   const faces = await faceDetector.detect(video);
   //ask browser for next animation and run detect for us
   faces.forEach(drawFace);
+  faces.forEach(censor);
   requestAnimationFrame(detect);
 }
 
@@ -38,7 +40,41 @@ function drawFace(face) {
   ctx.strokeStyle = "#ffc600";
   ctx.lineWidth = "2";
   ctx.strokeRect(left, top, width, height);
-  console.log(face);
+}
+
+function censor({ boundingBox: face }) {
+  faceCtx.imageSmoothingEnabled = false;
+  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+  // draw the small face
+  faceCtx.drawImage(
+    // 5 source arguments
+    video, // where does the source come from?
+    face.x, // where do we start the source pull from?
+    face.y,
+    face.width,
+    face.height,
+    // 4 draw arguments
+    face.x,
+    face.y,
+    SIZE,
+    SIZE
+  );
+  // take the face back out and draw it back at normal size
+
+  const width = face.width * SCALE;
+  const height = face.height * SCALE;
+  faceCtx.drawImage(
+    faceCanvas, // source
+    face.x,
+    face.y,
+    SIZE,
+    SIZE,
+    //drawing args
+    face.x - (width - face.width) / 2,
+    face.y - (height - face.height) / 2,
+    width,
+    height
+  );
 }
 
 populateVideo().then(detect);
